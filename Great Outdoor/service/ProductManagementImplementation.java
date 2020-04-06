5971aa6326cc5809a65e21ce4df21aeffc12ad39
punitrai <38477266+punitrai@users.noreply.github.com>
2020-04-06 13:46:21 +0530

    Add files via upload

---
+++ Great Outdoor/service/ProductManagementImplementation.java
@@ -0,0 +1,57 @@
+package com.cg.go.service;
+
+import java.util.ArrayList;
+
+
+import com.cg.go.bean.ProductManagementSystem;
+
+public class ProductManagementImplementation implements ProductManagement{
+    
+	ArrayList<ProductManagementSystem> ProSys= new ArrayList<ProductManagementSystem>();
+	
+	@Override
+	public void AddProduct(ProductManagementSystem Product) {
+		ProSys.add(Product);
+		// TODO Auto-generated method stub
+	}
+	
+	@Override
+	public boolean DeleteProduct(String ProductId) {
+		if(ProductId.equals(ProductId)) {
+		ProSys.remove(ProductId);
+			return true;
+		}
+		else
+          return false;
+	}
+
+	@Override
+	public boolean EditProductcolor(ProductManagementSystem P, String ProductId) {
+	    if(ProductId.equals(ProductId)) {
+	    	P.setColour(P.getColour());
+	        return true;
+	    }
+	    else
+		return false;
+	}
+
+	@Override
+	public boolean EditProductprice(ProductManagementSystem P, String ProductId) {
+		  if(ProductId.equals(ProductId)) {
+			  P.setPrice(P.getPrice());
+			  return true;
+		  }
+		return false;
+	}
+
+	@Override
+	public boolean EditProductQuantity(ProductManagementSystem P, String ProductId) {
+		if(ProductId.equals(ProductId)) {
+			P.setQuantity(P.getQuantity());
+			return true;
+		}
+		return false;
+	}
+	
+
+}
